8cfb0a35d5119375996b4aaf5e583eab62ce08fc
BaeCheolHan <joker10421@gmail.com>
2026-02-14 19:37:11 +0900

    chore(rust): stop tracking rust build artifacts

--- tools/rust_scanner/target/release/deps/sari_rust_scanner-7be8710b67a98dfe.d
+++
@@ -1,5 +0,0 @@
-/Users/baecheolhan/Documents/repositories/sari_project/tools/rust_scanner/target/release/deps/sari_rust_scanner-7be8710b67a98dfe.d: src/main.rs
-
-/Users/baecheolhan/Documents/repositories/sari_project/tools/rust_scanner/target/release/deps/sari_rust_scanner-7be8710b67a98dfe: src/main.rs
-
-src/main.rs:
--- tools/rust_scanner/target/release/sari_rust_scanner.d
+++
@@ -1,1 +0,0 @@
-/Users/baecheolhan/Documents/repositories/sari_project/tools/rust_scanner/target/release/sari_rust_scanner: /Users/baecheolhan/Documents/repositories/sari_project/tools/rust_scanner/src/main.rs
